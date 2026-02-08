88203573846d4ed853685224959f7a302a57b3a7
sudhanshu5669 <bhartiyasudhanshu5669@gmail.com>
2026-02-08 23:00:15 +0530

    Initialized the server with express. Replaced the old standalone initialization.

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const io = require('socket.io')
+const express = require('express')
+const { Server } = require('socket.io')
+const { createServer } = require('http')
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { });
+
+io.on('connection', (socket)=>{
+    console.log("A client connected: ", socket.id)
+})
+
+httpServer.listen(3000, ()=>{
+    console.log("Server started...")
+})
